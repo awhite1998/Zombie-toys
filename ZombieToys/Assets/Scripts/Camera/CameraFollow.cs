2bf42951f3f0c577204d16da5b22cdc2a42653dc
awhite1998 <awhite@IT002.mr.preble.com>
2015-09-11 13:35:10 -0400

    Code
    Camera

---
+++ ZombieToys/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5f;
+
+	Vector3 offset;
+
+	void Start()
+	{
+		offset = Transform.position - target.position;
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 targetCamPos = AvatarTarget.position + offset;
+		Transform.position = Vector3.Lerp (Transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
